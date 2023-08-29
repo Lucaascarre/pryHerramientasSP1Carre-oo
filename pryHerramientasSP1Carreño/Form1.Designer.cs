4d90932911d647eec20c8d6fe1b2a22630b512e5
Lucas <Lucas@DESKTOP-GTPEICM>
2023-08-29 01:09:07 -0300

    Agregar archivos de proyecto.

---
+++ pryHerramientasSP1Carreño/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace pryHerramientasSP1Carreño
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(12, 364);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(140, 50);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear Archivo";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(333, 364);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(140, 50);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer Archivo";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(29, 13);
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(432, 20);
+            this.txtDatos.TabIndex = 2;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(172, 188);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(140, 50);
+            this.btnCargar.TabIndex = 3;
+            this.btnCargar.Text = "Crear Archivo";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 426);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.TextBox txtDatos;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
+
---
+++ pryHerramientasSP1Carreño/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pryHerramientasSP1Carreño
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            StreamWriter SwCrearArchivo = new StreamWriter("MiArchivo", false);
+            if (SwCrearArchivo != null)
+            {
+                MessageBox.Show("Archivo creado");
+
+            }
+                
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            StreamReader SrManejoArchivo = new StreamReader ("MiArchivo");
+            while(SrManejoArchivo.EndOfStream==false)
+            {
+                txtDatos.Text += SrManejoArchivo.ReadLine() + Environment.NewLine;           
+            }
+            SrManejoArchivo.Close();
+        }
+    }
+}
